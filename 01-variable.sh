2c5e39ff02f1cb92c847f7d6567d37c52eb68bf7
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-04-22 19:35:03 +0530

    learning

--- 01-variable.sh
+++ 01-variable.sh
@@ -9,3 +9,9 @@ echo Today date is ${DATE}
 
 ARTH=$((2+3+5+6/2))
 echo ARTH = ${ARTH}
+
+echo Script Name - $0
+echo First Argument - $1
+echo Second Argument - $2
+echo All Argument - $*
+echo No of Arguments - $#
