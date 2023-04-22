877e7d1060687b10e3db1ff2909afea18676b571
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-04-22 19:11:13 +0530

    learning

--- 01-variable.sh
+++
@@ -1,7 +0,0 @@
-a=10,20,30
-name=devops
-
-echo a = $a
-echo name = ${name}
-
-DATE={$date +%F}
--- first.sh
+++
@@ -1,7 +0,0 @@
-mkdir repo
-echo "your directory has been created"
-touch repo/test{1..10}.txt
-echo "testing"
-ls -lst repo/*
-yum install nginx -y
-echo -e "\e[32/m<<<<<< install nginx >>>>\e[0m"
